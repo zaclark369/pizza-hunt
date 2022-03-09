72624214a3df2a5eaf6fc1df83a17f91d60cc113
Zachary Clark <zacclark369@gmail.com>
2022-03-08 18:55:09 -0800

    added more comment routes and controllers

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -21,7 +21,27 @@ const commentController = {
       .catch((err) => res.json(err));
   },
 
-  removeComment() {},
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({ _id: params.commentId })
+      .then(deletedComment => {
+        if (!deletedComment) {
+          return res.status(404).json({ message: 'No comment with this id!' });
+        }
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  }
 };
 
 module.exports = commentController;
